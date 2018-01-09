1622af24541b28a36c336b6cf873a9e155cabc36
Murgalha <murilobaldi1@gmail.com>
2018-01-09 16:33:16 -0200

    Update character sheet

--- Source/UI/char-sheet.cs
+++ Source/UI/char-sheet.cs
@@ -4,15 +4,23 @@ public static class CharSheet {
 
 	public static void Show(Hero H) {
 
-		Console.WriteLine("Name: " + H.Name + "\tRace: " + H.Race);
+		Console.WriteLine("\nName: " + H.Name + "\tRace: " + H.Race);
 		Console.WriteLine("HP: " + H.HP + "\t\tMP: " + H.MP);
 
 		Console.WriteLine("\nStrength: " + H.Strength + "\tFire Power: "+ H.FirePower);
 		Console.WriteLine("Resistance: " + H.Resistance+ "\tArmor: "+ H.Armor);
 		Console.WriteLine("Ability: " + H.Ability);
-	
-		Console.WriteLine("\nItems:");
+		
+		Console.WriteLine("\nGold: " + H.Gold);
+		Console.WriteLine("Items:");
 		foreach(var item in H.Bag.Inventory)
 			Console.WriteLine("> " + item.Name);
+
+		Console.WriteLine("\nEquipment:");
+		Console.WriteLine("> Weapon: " + H.Equip.Weapon);
+		Console.WriteLine("> Head: " + H.Equip.Head);
+		Console.WriteLine("> Torso: " + H.Equip.Torso);
+		Console.WriteLine("> Hand: " + H.Equip.Hand);
+		Console.WriteLine("> Leg: " + H.Equip.Leg);
 	} 
 }
