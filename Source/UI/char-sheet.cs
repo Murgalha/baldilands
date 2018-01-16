4751990c9af772019f194ba4303891379dc80a94
Murgalha <murilobaldi1@gmail.com>
2018-01-16 09:10:01 -0200

    Create FirstToUpper method to format names print

--- Source/UI/char-sheet.cs
+++ Source/UI/char-sheet.cs
@@ -2,6 +2,10 @@ using System;
 
 public static class CharSheet {
 
+	private static string FirstToUpper(string str) {
+		return (Char.ToUpper(str[0]) + str.Substring(1));
+	}
+
 	public static void Show(Hero H) {
 
 		Console.WriteLine("\nName: " + H.Name + "\tRace: " + H.Race);
@@ -14,13 +18,13 @@ public static class CharSheet {
 		Console.WriteLine("\nGold: " + H.Gold);
 		Console.WriteLine("Items:");
 		foreach(var item in H.Bag.Inventory)
-			Console.WriteLine("> " + item.Name);
+			Console.WriteLine("> " + FirstToUpper(item.Name));
 
 		Console.WriteLine("\nEquipment:");
-		Console.WriteLine("> Weapon: " + (H.Equip.Weapon != null ? H.Equip.Weapon.Name : ""));
-		Console.WriteLine("> Head: " + (H.Equip.Head != null ? H.Equip.Head.Name : ""));
-		Console.WriteLine("> Torso: " + (H.Equip.Torso != null ? H.Equip.Torso.Name : ""));
-		Console.WriteLine("> Hand: " + (H.Equip.Hand != null ? H.Equip.Hand.Name : ""));
-		Console.WriteLine("> Leg: " + (H.Equip.Leg != null ? H.Equip.Leg.Name : ""));
+		Console.WriteLine("> Weapon: " + (H.Equip.Weapon != null ? FirstToUpper(H.Equip.Weapon.Name) : ""));
+		Console.WriteLine("> Head: " + (H.Equip.Head != null ? FirstToUpper(H.Equip.Head.Name) : ""));
+		Console.WriteLine("> Torso: " + (H.Equip.Torso != null ? FirstToUpper(H.Equip.Torso.Name) : ""));
+		Console.WriteLine("> Hand: " + (H.Equip.Hand != null ? FirstToUpper(H.Equip.Hand.Name) : ""));
+		Console.WriteLine("> Leg: " + (H.Equip.Leg != null ? FirstToUpper(H.Equip.Leg.Name) : ""));
 	} 
 }
