d945e35eb3893be5e0d9cef7fbdc7a5b178aee09
Murilo Baldi <murilobaldi1@gmail.com>
2018-02-15 21:16:36 -0200

    Create monster initializer for battle

---
+++ Source/Initializer/initializer.cs
@@ -0,0 +1,12 @@
+public static class Initializer {
+
+	public static string[] InitMonsters() {
+		string[] monsters = {"abomination", "amoeba", "ankheg", "apparition", 
+			"archon", "atrociousvulture", "banshee", "basilisk", "beholder", 
+			"centaur", "darkdragon", "earthelemental", "giantbee", 
+			"giantcentipede", "gianteagle", "giantspider", "gryphbee", 
+			"infernalhound", "jellycube", "rattlingsniffer", "skeleton", 
+			"smasher", "voraciousbeetle", "walkingbush", "warhorse", "witch"};
+		 return monsters;
+	}
+}
