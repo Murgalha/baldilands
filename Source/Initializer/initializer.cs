756a3f5187f67fbad0e329d4bdead7335835a5cd
Murilo Baldi <murilobaldi1@gmail.com>
2018-02-18 19:09:04 -0300

    Add more monsters on Initializer and their files

--- Source/Initializer/initializer.cs
+++ Source/Initializer/initializer.cs
@@ -1,12 +1,7 @@
 public static class Initializer {
 
 	public static string[] InitMonsters() {
-		string[] monsters = {"abomination", "amoeba", "ankheg", "apparition", 
-			"archon", "atrociousvulture", "banshee", "basilisk", "beholder", 
-			"centaur", "darkdragon", "earthelemental", "giantbee", 
-			"giantcentipede", "gianteagle", "giantspider", "gryphbee", 
-			"infernalhound", "jellycube", "rattlingsniffer", "skeleton", 
-			"smasher", "voraciousbeetle", "walkingbush", "warhorse", "witch"};
-		 return monsters;
+		string[] monsters = {"abomination", "amoeba", "ankheg", "apparition", "archon", "atrociousvulture", "badger", "banshee", "basilisk", 			"beholder", "carnivorousgrass", "carrionworm", "caveworm", 				"centaur", "cyclope", "darkdragon", "earthelemental", "ent", "gargoyle","ghost", "giantbee", "giantcentipede", "gianteagle", "giantgrasshopper","giantscorpion", "giantspider", "goblinsoldier", "gryph", "gryphbee", "harpy", "hydra", "infernalhound", "jellycube","koboldguard", "kraken", "lich", "lion", "medusa", "mummy", "phoenix", "rattlingsniffer", "skeleton", "slark", "smasher", "tarrasque", "tentacute", "tiger", "voraciousbeetle", "walkingbush", "warhorse", "whitebear", "wisp", "witch", "wolf", "wyvern"};
+		return monsters;
 	}
 }
