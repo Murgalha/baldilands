555ed781d3014b69ccd9d1229bcc192e50fa9154
Murgalha <murilobaldi1@gmail.com>
2018-02-26 21:36:34 -0300

    Create FileCreator class for monsters

---
+++ Source/Meta/filecreator.cs
@@ -0,0 +1,64 @@
+public class FileCreator {
+
+	public static void Monsters() {
+		Bestiary.Make("Giant Bee", 1, 3, 0, 0, 0, false);
+		Bestiary.Make("Abomination", 2, 3, 4, 1, 0, false);
+		Bestiary.Make("Rattling Sniffer", 1, 1, 3, 1, 1, true);
+		Bestiary.Make("Gryph Bee", 2, 2, 2, 3, 0, false);
+		Bestiary.Make("Atrocious Vulture", 1, 2, 1, 0, 0, false);
+		Bestiary.Make("Giant Eagle", 3, 3, 3, 3, 0, false);
+		Bestiary.Make("Amoeba", 4, 1, 6, 1, 0, false);
+		Bestiary.Make("Ankheg", 2, 1, 2, 2, 3, true);
+		Bestiary.Make("Apparition", 3, 2, 2, 1, 0, false);
+		Bestiary.Make("Giant Spider", 1, 3, 1, 0, 1, true);
+		Bestiary.Make("Walking Bush", 4, 1, 2, 2, 0, false);
+		Bestiary.Make("Archon", 0, 3, 1, 0, 5, true);
+		Bestiary.Make("Banshee", 3, 4, 3, 4, 4, true);
+		Bestiary.Make("Basilisk", 0, 3, 2, 1, 1, true);
+		Bestiary.Make("Beholder", 3, 3, 5, 5, 6, true);
+		Bestiary.Make("Voracious Beetle", 4, 1, 18, 2, 0, false);
+		Bestiary.Make("Witch", 3, 0, 3, 3, 0, false);
+		Bestiary.Make("War Horse", 2, 1, 2, 1, 0, false);
+		Bestiary.Make("Infernal Hound", 2, 2, 2, 2, 4, true);
+		Bestiary.Make("Centaur", 4, 3, 3, 1, 1, false);
+		Bestiary.Make("Giant Centipede", 3, 3, 2, 1, 0, false);
+		Bestiary.Make("Jelly Cube", 1, 0, 5, 0, 0, false);
+		Bestiary.Make("Dark Dragon", 4, 4, 4, 5, 9, true);
+		Bestiary.Make("Earth Elemental", 1, 4, 3, 1, 1, true);
+		Bestiary.Make("Smasher", 3, 2, 3, 3, 0, false);
+		Bestiary.Make("Skeleton", 1, 0, 0, 2, 0, false);
+		Bestiary.Make("Ent", 7, 3, 12, 5, 0, false);
+		Bestiary.Make("Giant Scorpion", 2, 2, 3, 2, 0, false);
+		Bestiary.Make("Ghost", 1, 1, 2, 2, 4, true);
+		Bestiary.Make("Phoenix", 5, 6, 7, 6, 8, true);
+		Bestiary.Make("Wisp", 0, 4, 0, 0, 0, false);
+		Bestiary.Make("Giant Grasshopper", 1, 3, 2, 1, 1, false);
+		Bestiary.Make("Gargoyle", 3, 5, 4, 4, 0, false);
+		Bestiary.Make("Cyclope", 20, 4, 9, 7, 18, false);
+		Bestiary.Make("Goblin Soldier", 0, 1, 1, 0, 1, true);
+		Bestiary.Make("Carnivorous Grass", 0, 1, 0, 0, 0, false);
+		Bestiary.Make("Lion", 3, 3, 2, 0, 0, false);
+		Bestiary.Make("Tiger", 2, 3, 2, 0, 0, false);
+		Bestiary.Make("Gryph", 2, 7, 3, 1, 0, false);
+		Bestiary.Make("Harpy", 2, 5, 3, 1, 3, true);
+		Bestiary.Make("Hydra", 4, 3, 10, 4, 10, true);
+		Bestiary.Make("Kobold Guard", 0, 1, 0, 1, 0, false);
+		Bestiary.Make("Kraken", 7, 5, 10, 6, 0, false);
+		Bestiary.Make("Lich", 3, 5, 5, 6, 5, true);
+		Bestiary.Make("Wolf", 1, 2, 1, 0, 0, false);
+		Bestiary.Make("Medusa", 1, 2, 3, 0, 6, false);
+		Bestiary.Make("Mummy", 4, 1, 3, 4, 0, false);
+		Bestiary.Make("Slark", 0, 0, 0, 0, 0, false);
+		Bestiary.Make("Tarrasque", 8, 5, 8, 10, 0, false);
+		Bestiary.Make("Badger", 2, 1, 2, 1, 0, false);
+		Bestiary.Make("Tentacute", 0, 0, 0, 0, 0, true);
+		Bestiary.Make("White Bear", 3, 2, 3, 1, 0, false);
+		Bestiary.Make("Wyvern", 5, 5, 4, 4, 3, false);
+		Bestiary.Make("Cave Worm", 10, 1, 30, 4, 0, false);
+		Bestiary.Make("Carrion Worm", 1, 2, 2, 2, 0, false);
+	}
+
+	public static void Items() {
+
+	}
+}
